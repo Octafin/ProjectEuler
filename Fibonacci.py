db45758f7cc49e16b8456ba3b8b845fce64e8c78
Octafin <leoandrebenezit@gmail.com>
2017-05-08 15:53:00 +0100

    Problem 2
    Even Fibonacci sum

---
+++ Fibonacci.py
@@ -0,0 +1,19 @@
+fibonacci = [1,2]
+x = fibonacci[-1]
+even = []
+
+while fibonacci[-1] + fibonacci[-2] < 4000000:
+
+
+    x = fibonacci[-1] + fibonacci[-2]
+
+    fibonacci.append(x)
+
+
+for x in fibonacci:
+    if x % 2 == 0:
+        even.append(x)
+
+
+
+print(sum(even))
